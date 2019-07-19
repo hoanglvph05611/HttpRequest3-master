a4a52b3af4d2a203700de5b58081eefa74fa35c2
Hoanglvph05611 <hoanglvph05611@fpt.edu.vn>
2019-07-19 08:43:00 +0700

    Initial commit

---
+++ app/src/main/java/com/d/httprequest/Post.java
@@ -0,0 +1,51 @@
+package com.d.httprequest;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    public int id;
+    public String title;
+    public String date;
+
+    public Post() {
+    }
+
+    public Post(int id, String title, String date) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", date='" + date + '\'' +
+                '}';
+    }
+}
